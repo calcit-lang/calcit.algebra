201cafc7f53c90fc39ce517d4019e7c4866ca6f2
jiyinyiyong <jiyinyiyong@gmail.com>
2021-09-28 13:06:14 +0800

    fix CI script

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import { main_$x_, reload_$x_ } from "./js-out/app.main.js"
+import { main_$x_, reload_$x_ } from "./js-out/algebra.test.js"
 
 main_$x_()
 
 if (module.hot) {
-  module.hot.accept('./js-out/app.main.js', (main) => {
+  module.hot.accept('./js-out/algebra.test.js', (main) => {
     reload_$x_()
   })
 }
